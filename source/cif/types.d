24d7ae3bc1c60dbb201dcb79d1ef138440845871
Scott Raine <me@nylar.io>
2017-12-18 18:08:34 +0000

    Added none cases to enumerations

--- source/cif/types.d
+++ source/cif/types.d
@@ -2,18 +2,21 @@ module cif.types;
 
 enum AssociationType : char
 {
+    none = ' ',
     passengerUse = 'P',
     operatingUseOnly = 'O',
 }
 
 enum NoteType : char
 {
+    none = ' ',
     GBTT = 'G',
     WTT = 'W',
 }
 
 enum TransactionType : char
 {
+    none = ' ',
     new_ = 'N',
     delete_ = 'D',
     revise = 'R',
@@ -21,6 +24,7 @@ enum TransactionType : char
 
 enum UpdateType : char
 {
+    none = ' ',
     update = 'U',
     full = 'F',
 }
